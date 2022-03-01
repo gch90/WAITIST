12d12ba1bbac709fb29aaa59e03b09f3dddbe4dd
Georges Chami <g.chami90@gmail.com>
2022-03-01 15:46:12 -0500

    created application and fixed role column name for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create(email: "s@s.s", password: "qwerty", first_name: "Sedrak", last_name:
 User.create(email: "m@m.m", password: "qwerty", first_name: "Martyn", last_name: "McGowan", address: "5333 Av. Casgrain Apt 103", description: "new waiter")
 User.create(email: "r@r.r", password: "qwerty", first_name: "Raphael", last_name: "Cohen", address: "5333 Av. Casgrain Apt 104", description: "new waiter")
 
-User.create(role: 1, email: "g1@g.g", password: "qwerty", restaurant_name: "Renoir Restaurant", address: "1155 Serbrook", description: "Eclectic-New French recipes offered in a high-end hotel restaurant with a terrace.", rate: 4.5)
-User.create(role: 1, email: "s1@s.s", password: "qwerty", restaurant_name: "Estiatorio Milos", address: "5357 Park Ave ", description: "Stylish Greek taverna with fresh seafood & a popular fixed-price lunch in a whitewashed setting.", rate: 4.6)
-User.create(role: 1, email: "m1@m.m", password: "qwerty", restaurant_name: "Baton Rouge", address: "5385 Rue des Jockeys", description: "Bâton Rouge Steakhouse & Bar is a Canadian restaurant chain", rate: 4.2)
-User.create(role: 1, email: "r1@r.r", password: "qwerty", restaurant_name: "Bis", address: "1229 Rue de la Montagne", description: "Classic Italian cuisine, with gluten-free pastas & vegetarian choices, in a homey & elegant space.", rate: 3.9)
+User.create(user_role: 1, email: "g1@g.g", password: "qwerty", restaurant_name: "Renoir Restaurant", address: "1155 Serbrook", description: "Eclectic-New French recipes offered in a high-end hotel restaurant with a terrace.", rate: 4.5)
+User.create(user_role: 1, email: "s1@s.s", password: "qwerty", restaurant_name: "Estiatorio Milos", address: "5357 Park Ave ", description: "Stylish Greek taverna with fresh seafood & a popular fixed-price lunch in a whitewashed setting.", rate: 4.6)
+User.create(user_role: 1, email: "m1@m.m", password: "qwerty", restaurant_name: "Baton Rouge", address: "5385 Rue des Jockeys", description: "Bâton Rouge Steakhouse & Bar is a Canadian restaurant chain", rate: 4.2)
+User.create(user_role: 1, email: "r1@r.r", password: "qwerty", restaurant_name: "Bis", address: "1229 Rue de la Montagne", description: "Classic Italian cuisine, with gluten-free pastas & vegetarian choices, in a homey & elegant space.", rate: 3.9)
