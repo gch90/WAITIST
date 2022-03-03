54da5f60f9dbc50d66293cd029dfcdf62faa968d
Sedrak Nadzharyan <sedrak005@gmail.com>
2022-03-03 14:04:35 -0500

    card done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @restaurant = User.find(user_role: 1)
+    @restaurant = User.restaurant[0]
   end
 end
