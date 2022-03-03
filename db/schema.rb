c84b370c57e5c815d4bff7adfaa30061ab25cbf9
Georges Chami <g.chami90@gmail.com>
2022-03-03 11:44:01 -0500

    fixed user model, changed enum 0 to waiter

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2022_03_02_220049) do
   enable_extension "plpgsql"
 
   create_table "applications", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "waiter_id"
     t.bigint "restaurant_id"
     t.date "start_date"
     t.date "end_date"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2022_03_02_220049) do
     t.bigint "creator_id"
     t.index ["creator_id"], name: "index_applications_on_creator_id"
     t.index ["restaurant_id"], name: "index_applications_on_restaurant_id"
-    t.index ["user_id"], name: "index_applications_on_user_id"
+    t.index ["waiter_id"], name: "index_applications_on_waiter_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,16 +41,18 @@ ActiveRecord::Schema.define(version: 2022_03_02_220049) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
-    t.string "description"
-    t.integer "role", default: 0
+    t.text "description"
+    t.integer "user_role", default: 0
     t.float "rate", default: 0.0
     t.string "restaurant_name"
+    t.string "restaurant_type"
+    t.string "summary"
     t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "applications", "users"
   add_foreign_key "applications", "users", column: "creator_id"
   add_foreign_key "applications", "users", column: "restaurant_id"
+  add_foreign_key "applications", "users", column: "waiter_id"
 end
