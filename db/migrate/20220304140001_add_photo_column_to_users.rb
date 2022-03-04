819c1c2347da17ed3e8baeafc1b390a4c8401054
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-03-04 12:22:42 -0500

    WIP

---
+++ db/migrate/20220304140001_add_photo_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddPhotoColumnToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :photo, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_220733) do
+ActiveRecord::Schema.define(version: 2022_03_04_162039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2022_03_03_220733) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "applications", "users", column: "waiter_id"
+  add_foreign_key "applications", "users"
   add_foreign_key "applications", "users", column: "creator_id"
   add_foreign_key "applications", "users", column: "restaurant_id"
 end
