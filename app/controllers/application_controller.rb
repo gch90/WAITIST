eed52ac12c2168e21c2b3c1a8b19c8fa5c4b8582
Georges Chami <g.chami90@gmail.com>
2022-03-04 14:09:26 -0500

    completed and fixed cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # For additional fields in app/views/devise/registrations/new.html.erb
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :description, :rate, :restaurant_name, photos: []])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :description, :rate, :restaurant_name, :avatar, photos: [] ])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :description, :rate, :restaurant_name])
