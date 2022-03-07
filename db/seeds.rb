a4855e60fc97e2a21c5a14c4033bbf581705851a
Raphael Cohen <raphaelcohen007@gmail.com>
2022-03-07 12:26:27 -0500

    added images and faker to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,50 +13,48 @@ waiter_three = User.create!(user_role: 0, email: "m@m.m", password: "qwerty", fi
 waiter_four = User.create!(user_role: 0, email: "r@r.r", password: "qwerty", first_name: "Raphael", last_name: "Cohen", address: "chemin Guelph, Montreal", description: "new waiter", phone: "555-514-1234")
 # restaurants owners
 
-waiter_one.avatar.attach(io: File.open('app/assets/images/georges.jpg'), filename: 'georges.jpg', content_type: 'image/jpg')
+waiter_one.avatar.attach(io: File.open('app/assets/images/waiters/georges.jpg'), filename: 'georges.jpg', content_type: 'image/jpg')
 waiter_one.save!
-waiter_two.avatar.attach(io: File.open('app/assets/images/sedrak.jpg'), filename: 'sedrak.jpg', content_type: 'image/jpg')
+waiter_two.avatar.attach(io: File.open('app/assets/images/waiters/sedrak.jpg'), filename: 'sedrak.jpg', content_type: 'image/jpg')
 waiter_two.save!
-waiter_three.avatar.attach(io: File.open('app/assets/images/martyn.jpg'), filename: 'martyn.jpg', content_type: 'image/jpg')
+waiter_three.avatar.attach(io: File.open('app/assets/images/waiters/martyn.jpg'), filename: 'martyn.jpg', content_type: 'image/jpg')
 waiter_three.save!
-waiter_four.avatar.attach(io: File.open('app/assets/images/raphael.jpg'), filename: 'raphael.jpg', content_type: 'image/jpg')
+waiter_four.avatar.attach(io: File.open('app/assets/images/waiters/raphael.jpg'), filename: 'raphael.jpg', content_type: 'image/jpg')
 waiter_four.save!
 
 
 p "creating restos"
 
 # creating more seeds for the restaurant owners
-all_restos = []
-all_restos << User.create!(user_role: 1, email: "1@1.r", password: "qwerty", restaurant_name: "Trattoria Bellamore", address: "1229 Rue de la Montagne, Montreal", restaurant_type: "Italian", summary: "White-tablecloth Italian BYOB serving classic meat, seafood & pasta dishes in a quaint space.", rate: 15, description: "
-Lorem ipsum", phone: "555-514-1234", website: "https://trattoriabellamore.com/")
-all_restos << User.create!(user_role: 1, email: "2@2.r", password: "qwerty", restaurant_name: "Le Mitoyen", address: "652 Rue de la Place Publique, Laval", restaurant_type: "Canadian", summary: "Area-sourced Québec tasting menus starring game & decadent desserts in a genteel country house.", rate: 16, description: "
-Lorem ipsum", phone: "555-514-1234" , website: "https://restaurantlemitoyen.com/" )
-all_restos << User.create!(user_role: 1, email: "3@3.r", password: "qwerty", restaurant_name: "Sterlings Steakhouse", address: "165 Prom. Du Centropolis, Laval", restaurant_type: "American", summary: "Aged USDA prime steaks, fresh fish & a private wine cellar are offered in a sophisticated setting.", rate: 16, description: "
-Lorem ipsum", phone: "555-514-1234", website: "http://www.sterlingsrestaurant.com/")
-all_restos << User.create!(user_role: 1, email: "4@4.r", password: "qwerty", restaurant_name: "Le Vieux Four de Laval", address: "St-Vincent-de-Paul, 5070 Bd Lévesque E, Laval", restaurant_type: "Italian", summary: "Sprawling family eatery for wood-fired, thin-crust pizzas, plus pastas & Italian classics.", rate: 18, description: "
-Lorem ipsum", phone: "555-514-1234", website: "http://www.levieuxfour.net/")
-all_restos << User.create!(user_role: 1, email: "5@5.r", password: "qwerty", restaurant_name: "Restaurant Amato", address: "192 Bd Sainte-Rose, Laval", restaurant_type: "Italian", summary: "Set in an 1895 house, this refined white-tablecloth restaurant serves traditional Italian cuisine.", rate: 18, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "6@6.r", password: "qwerty", restaurant_name: "La Serenata Restaurant", address: "53 Brunswick Blvd, Dollard-Des Ormeaux, Montreal", restaurant_type: "Italian", summary: "Fixture offering Italian wines & traditional dishes with a serenading accordion player on weekends.", rate: 19, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "7@7.r", password: "qwerty", restaurant_name: "Enoteca Monza Pizzeria Moderna", address: "3800 Boul Saint-Jean, Dollard-des-Ormeaux, Montreal", restaurant_type: "Italian", summary: "Polished Italian specialist for wood-fired panini & pizza, plus classic dishes & wines by the glass.", rate: 18, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "8@8.r", password: "qwerty", restaurant_name: "Del Friscos", address: "3237 Sources Blvd, Dollard-Des Ormeaux, Montreal", restaurant_type: "Italian", summary: "Hefty portions of familiar pastas & grilled meats (plus wood-oven pizzas) in an inviting room.", rate: 17, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "9@9.r", password: "qwerty", restaurant_name: "Madisons Restaurant & Bar", address: "11590 Salaberry Blvd, Dollard-Des Ormeaux, Montreal", restaurant_type: "Canadian", summary: "cozy place. nice service. delicious food ...", rate: 15, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "10@10.r", password: "qwerty", restaurant_name: "Pastamore", address: "M-3343 Sources Blvd, Dollard-Des Ormeaux, Montreal", restaurant_type: "Italian", summary: "Good portions, great atmosphere, great service at a great price", rate: 14, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "11@11.r", password: "qwerty", restaurant_name: "Renoir Restaurant", address: "1155 Serbrook, Montreal", restaurant_type: "French", summary: "Eclectic-New French recipes offered in a high-end hotel restaurant with a terrace.", rate: 15, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "12@12.r", password: "qwerty", restaurant_name: "Estiatorio Milos", address: "5357 Park Ave, Montreal", restaurant_type: "Greek", summary: "Stylish Greek taverna with fresh seafood & a popular fixed-price lunch in a whitewashed setting.", rate: 16, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "13@13.r", password: "qwerty", restaurant_name: "Baton Rouge", address: "5385 Rue des Jockeys, Montreal", restaurant_type: "Canadian", summary: "Bâton Rouge Steakhouse & Bar is a Canadian restaurant chain", rate: 18, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
-all_restos << User.create!(user_role: 1, email: "14@14.r", password: "qwerty", restaurant_name: "Bis", address: "1229 Rue de la Montagne, Montreal", restaurant_type: "Italian", summary: "Classic Italian cuisine, with gluten-free pastas & vegetarian choices, in a homey & elegant space.", rate: 16, description: "
-Lorem ipsum", phone: "555-514-1234", website:"https://www.google.com/")
+my_restos = []
+my_restos << User.create!(user_role: 1, email: "1@1.r", password: "qwerty", restaurant_name: "Trattoria Bellamore", address: "1229 Rue de la Montagne, Montreal", restaurant_type: "Italian", summary: "White-tablecloth Italian BYOB serving classic meat, seafood & pasta dishes in a quaint space.", rate: 15, description: Faker::Restaurant.description, phone: "555-514-1234", website: "https://trattoriabellamore.com/")
+my_restos << User.create!(user_role: 1, email: "2@2.r", password: "qwerty", restaurant_name: "Le Mitoyen", address: "652 Rue de la Place Publique, Laval", restaurant_type: "Canadian", summary: "Area-sourced Québec tasting menus starring game & decadent desserts in a genteel country house.", rate: 16, description: Faker::Restaurant.description, phone: "555-514-1234" , website: "https://restaurantlemitoyen.com/" )
+my_restos << User.create!(user_role: 1, email: "3@3.r", password: "qwerty", restaurant_name: "Sterlings Steakhouse", address: "165 Prom. Du Centropolis, Laval", restaurant_type: "American", summary: "Aged USDA prime steaks, fresh fish & a private wine cellar are offered in a sophisticated setting.", rate: 16, description: Faker::Restaurant.description, phone: "555-514-1234", website: "http://www.sterlingsrestaurant.com/")
+my_restos << User.create!(user_role: 1, email: "4@4.r", password: "qwerty", restaurant_name: "Le Vieux Four de Laval", address: "St-Vincent-de-Paul, 5070 Bd Lévesque E, Laval", restaurant_type: "Italian", summary: "Sprawling family eatery for wood-fired, thin-crust pizzas, plus pastas & Italian classics.", rate: 18, description: Faker::Restaurant.description, phone: "555-514-1234", website: "http://www.levieuxfour.net/")
 
-nums = (1..4).to_a
+
+all_restos = []
+all_restos << User.create!(user_role: 1, email: "5@5.r", password: "qwerty", restaurant_name: "Restaurant Amato", address: "192 Bd Sainte-Rose, Laval", restaurant_type: "Italian", summary: "Set in an 1895 house, this refined white-tablecloth restaurant serves traditional Italian cuisine.", rate: 18, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "6@6.r", password: "qwerty", restaurant_name: "La Serenata Restaurant", address: "53 Brunswick Blvd, Dollard-Des Ormeaux, Montreal", restaurant_type: "Italian", summary: "Fixture offering Italian wines & traditional dishes with a serenading accordion player on weekends.", rate: 19, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "7@7.r", password: "qwerty", restaurant_name: "Enoteca Monza Pizzeria Moderna", address: "3800 Boul Saint-Jean, Dollard-des-Ormeaux, Montreal", restaurant_type: "Italian", summary: "Polished Italian specialist for wood-fired panini & pizza, plus classic dishes & wines by the glass.", rate: 18, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "8@8.r", password: "qwerty", restaurant_name: "Del Friscos", address: "3237 Sources Blvd, Dollard-Des Ormeaux, Montreal", restaurant_type: "Italian", summary: "Hefty portions of familiar pastas & grilled meats (plus wood-oven pizzas) in an inviting room.", rate: 17, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "9@9.r", password: "qwerty", restaurant_name: "Madisons Restaurant & Bar", address: "11590 Salaberry Blvd, Dollard-Des Ormeaux, Montreal", restaurant_type: "Canadian", summary: "cozy place. nice service. delicious food ...", rate: 15, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "10@10.r", password: "qwerty", restaurant_name: "Pastamore", address: "M-3343 Sources Blvd, Dollard-Des Ormeaux, Montreal", restaurant_type: "Italian", summary: "Good portions, great atmosphere, great service at a great price", rate: 14, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "11@11.r", password: "qwerty", restaurant_name: "Renoir Restaurant", address: "1155 Serbrook, Montreal", restaurant_type: "French", summary: "Eclectic-New French recipes offered in a high-end hotel restaurant with a terrace.", rate: 15, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "12@12.r", password: "qwerty", restaurant_name: "Estiatorio Milos", address: "5357 Park Ave, Montreal", restaurant_type: "Greek", summary: "Stylish Greek taverna with fresh seafood & a popular fixed-price lunch in a whitewashed setting.", rate: 16, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "13@13.r", password: "qwerty", restaurant_name: "Baton Rouge", address: "5385 Rue des Jockeys, Montreal", restaurant_type: "Canadian", summary: "Bâton Rouge Steakhouse & Bar is a Canadian restaurant chain", rate: 18, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+all_restos << User.create!(user_role: 1, email: "14@14.r", password: "qwerty", restaurant_name: "Bis", address: "1229 Rue de la Montagne, Montreal", restaurant_type: "Italian", summary: "Classic Italian cuisine, with gluten-free pastas & vegetarian choices, in a homey & elegant space.", rate: 16, description: Faker::Restaurant.description, phone: Faker::Config.locale = 'en-CA', website: Faker::Internet.url)
+
+
+my_restos.each_with_index do |user, resto_num|
+  5.times do |i|
+    p i
+    user.photos.attach(io: File.open("app/assets/images/my_restos/resto_#{resto_num+1}_#{i+1}.jpg"), filename: "resto#{resto_num+1}_#{i+1}.jpg", content_type: 'image/jpg')
+    user.save!
+  end
+end
+
+nums = (5..14).to_a
 all_restos.each do |user|
   5.times do
     num = nums.sample
@@ -64,6 +62,8 @@ all_restos.each do |user|
     user.save!
   end
 end
+
+
 # resto_one.photos.attach(io: File.open('app/assets/images/resto1.jpg'), filename: 'resto1.jpg', content_type: 'image/jpg')
 # resto_one.save!
 # resto_two.photos.attach(io: File.open('app/assets/images/resto2.jpg'), filename: 'resto2.jpg', content_type: 'image/jpg')
