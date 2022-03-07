8cf458d7bf3e4f6bcedadaf25adf05c42d4601f8
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-03-06 23:23:27 -0500

    WIP chatbox, needs broadcast review. needs button to create a room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_06_183830) do
+ActiveRecord::Schema.define(version: 2022_03_07_010233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,22 @@ ActiveRecord::Schema.define(version: 2022_03_06_183830) do
     t.index ["waiter_id"], name: "index_applications_on_waiter_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,6 +95,7 @@ ActiveRecord::Schema.define(version: 2022_03_06_183830) do
     t.string "website"
     t.float "latitude"
     t.float "longitude"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -88,4 +105,6 @@ ActiveRecord::Schema.define(version: 2022_03_06_183830) do
   add_foreign_key "applications", "users", column: "creator_id"
   add_foreign_key "applications", "users", column: "restaurant_id"
   add_foreign_key "applications", "users", column: "waiter_id"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
