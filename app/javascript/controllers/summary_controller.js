6664b9bdd616245eda5b3c1330e967d14111b2e4
Georges Chami <g.chami90@gmail.com>
2022-03-09 12:03:26 -0500

    did ratings

--- app/javascript/controllers/summary_controller.js
+++ app/javascript/controllers/summary_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["summary"];
+  static targets = ["summary", "ratings"];
   connect() {}
 
   open(e) {
@@ -14,13 +14,14 @@ export default class extends Controller {
     this.summaryTarget.classList.toggle("fade-in");
   }
 
-  open(e) {
+  open_ratings(e) {
     if (e.target.classList.contains("arrow")) {
       e.target.classList.toggle("rotate");
     } else {
       e.target.parentNode.classList.toggle("rotate");
     }
-    this.summaryTarget.classList.toggle("d-none");
-    this.summaryTarget.classList.toggle("fade-in");
+    this.ratingsTarget.classList.toggle("d-none");
+    this.ratingsTarget.classList.toggle("fade-in");
   }
+
 }
