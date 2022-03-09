5fd3d0aa0c176c4ccbed989c2e7d19cca69b30f6
Georges Chami <g.chami90@gmail.com>
2022-03-09 15:23:10 -0500

    completed change date and application if empty

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,6 +3,9 @@ class Application < ApplicationRecord
   belongs_to :restaurant, class_name: "User"
   belongs_to :creator, class_name: "User"
 
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
   enum status: {
     pending: 0,
     approved: 1,
