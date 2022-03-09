8529d1e82c25b1c298fa3f80989210061a4ea99b
Sedrak Nadzharyan <sedrak005@gmail.com>
2022-03-09 09:09:36 -0500

    clean migrations

--- db/migrate/20220309004732_change_application_date_names.rb
+++ db/migrate/20220309004732_change_application_date_names.rb
@@ -1,9 +1,9 @@
 class ChangeApplicationDateNames < ActiveRecord::Migration[6.1]
   def change
-    change_column :applications, :start_time, :datetime
-    change_column :applications, :end_time, :datetime
-    rename_column :applications, :start_time, :start_time
-    rename_column :applications, :end_time, :end_time
+    change_column :applications, :start_date, :datetime
+    change_column :applications, :end_date, :datetime
+    rename_column :applications, :start_date, :start_time
+    rename_column :applications, :end_date, :end_time
 
   end
 end
