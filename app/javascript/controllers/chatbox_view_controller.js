b2e00048daaa8532c45f850904a4da80ba26a1b8
Sedrak Nadzharyan <sedrak005@gmail.com>
2022-03-10 12:07:17 -0500

    messages

--- app/javascript/controllers/chatbox_view_controller.js
+++ app/javascript/controllers/chatbox_view_controller.js
@@ -1,19 +1,47 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["chatbox", "close"]
+  static targets = ["pages", "tabs"];
 
   connect() {
-    console.log('connected');
+    console.log(this.pagesTarget);
+    this.initialTab(0);
   }
 
-  showChat() {
-    this.chatboxTarget.classList.remove("d-none");
-    this.closeTarget.classList.remove("d-none");
+  select(event) {
+    const num = this.tabStyleSwitch(event);
+    this.chosePage(num);
   }
 
-  hideChat() {
-    this.chatboxTarget.classList.add("d-none");
-    this.closeTarget.classList.add("d-none");
+  initialTab(num) {
+    const tabSelected = this.tabsTarget.children[num];
+    const pageSelected = this.pagesTarget.children[num];
+    tabSelected.classList.add("active");
+    pageSelected.classList.remove("d-none");
+  }
+
+  tabStyleSwitch(event) {
+    const tabSelected = event.target.classList.contains("tab")
+      ? event.target
+      : event.target.parentNode;
+
+    const tabs = Array.from(tabSelected.parentNode.children);
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabSelected.classList.add("active");
+    console.log(tabs);
+    console.log(tabSelected);
+    return tabs.indexOf(tabSelected);
+  }
+
+  chosePage(num) {
+    const pageSelected = this.pagesTarget.children[num];
+
+    const pages = Array.from(this.pagesTarget.children);
+    pages.forEach((page) => {
+      page.classList.add("d-none");
+    });
+    pageSelected.classList.remove("d-none");
   }
 }
